eff8eaab697029b23f7fb54c74674511719778af
Valery Piashchynski <piashchynski.valery@gmail.com>
2022-01-01 12:10:02 +0300

    upd
    Signed-off-by: Valery Piashchynski <piashchynski.valery@gmail.com>

--- simple/main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by valery on 27/11/2021.
-//
-
-#ifndef OPENCV_PLAYGROUND_MAIN_H
-#define OPENCV_PLAYGROUND_MAIN_H
-
-#endif //OPENCV_PLAYGROUND_MAIN_H
--- video/main.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by valery on 28/11/2021.
-//
-
-#ifndef OPENCV_PLAYGROUND_MAIN_H
-#define OPENCV_PLAYGROUND_MAIN_H
-
-#endif //OPENCV_PLAYGROUND_MAIN_H
